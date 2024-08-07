8e6ff3408eb2b8ddc7549a1928d6f698aae3753f
Joon2000 <najoonwon22@gmail.com>
2024-08-07 18:53:02 +0900

    Add private key

--- scripts/deploy/V3/DeployFiatTokenV3Script.s.sol
+++ scripts/deploy/V3/DeployFiatTokenV3Script.s.sol
@@ -6,12 +6,18 @@ import { Script } from "forge-std/Script.sol";
 import { FiatTokenV3 } from "../../../contracts/v3/FiatTokenV3.sol";
 
 contract DeployFiatTokenV3Script is Script {
+    uint256 private deployerPrivateKey;
+
     address trustedForwarder = address(
         0x71bB75dCB43f57360b0e2fdd08BE14622741FD80
     );
 
+    function setUp() public {
+        deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+    }
+
     function run() external {
-        vm.startBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
 
         FiatTokenV3 fiatToken = new FiatTokenV3(trustedForwarder);
 
