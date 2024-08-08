1490377b1be1b8def17aee31bd7d2889add7adfc
Joon2000 <najoonwon22@gmail.com>
2024-08-08 15:33:04 +0900

    Add ERC2771Context

--- contracts/v3/FiatTokenV3.sol
+++ contracts/v3/FiatTokenV3.sol
@@ -3,6 +3,7 @@ pragma solidity 0.6.12;
 
 import { FiatTokenV2_2 } from "./../v2/FiatTokenV2_2.sol";
 import { ERC2771Context } from "./ERC2771Context.sol";
+import { Context } from "@openzeppelin/contracts/GSN/Context.sol";
 
 contract FiatTokenV3 is FiatTokenV2_2, ERC2771Context {
     constructor(address trustedForwarder)
@@ -13,7 +14,7 @@ contract FiatTokenV3 is FiatTokenV2_2, ERC2771Context {
     function _msgSender()
         internal
         virtual
-        override(ERC2771Context)
+        override(ERC2771Context, Context)
         view
         returns (address payable)
     {
@@ -23,7 +24,7 @@ contract FiatTokenV3 is FiatTokenV2_2, ERC2771Context {
     function _msgData()
         internal
         virtual
-        override(ERC2771Context)
+        override(ERC2771Context, Context)
         view
         returns (bytes memory)
     {
